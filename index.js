4475ceec4694c63593abdb469e544cf44c92f9d0
Jeff Kao <jeff.kao.000@gmail.com>
2022-12-29 14:32:38 -0800

    Update to node 18 with package upgrades

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // magic incantation from step 3 @ https://github.com/mapbox/node-pre-gyp#readme
-const binary = require('node-pre-gyp');
+const binary = require('@mapbox/node-pre-gyp');
 const path = require('path');
 var binding_path = binary.find(path.resolve(path.join(__dirname,'./package.json')));
 const s2 = require(binding_path);
