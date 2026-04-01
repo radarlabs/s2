41447123d26d473fa1d7dc0a3adcbc4ab37f12cd
Binh 🌱 <binhrobles@gmail.com>
2026-04-01 13:10:15 -0400

    init

--- publish-linux.sh
+++ publish-linux.sh
@@ -2,17 +2,19 @@
 
 set -ex
 
-export DOCKER_DEFAULT_PLATFORM="${DOCKER_DEFAULT_PLATFORM:-linux/amd64}"
-
-for node in 20.0.0 22.0.0 24.0.0
+for platform in linux/amd64 linux/arm64
 do
-  # run image
-  docker run -it \
-    -v "$PWD":/app \
-    -e AWS_DEFAULT_REGION \
-    -e AWS_ACCESS_KEY_ID \
-    -e AWS_SECRET_ACCESS_KEY \
-    -e AWS_SESSION_TOKEN \
-    node:$node \
-    bash -c 'cd /app && npm ci && JOBS=max npx --no-install node-pre-gyp build package unpublish publish'
+  for node in 20.0.0 22.0.0 24.0.0
+  do
+    # run image
+    docker run -it \
+      --platform "$platform" \
+      -v "$PWD":/app \
+      -e AWS_DEFAULT_REGION \
+      -e AWS_ACCESS_KEY_ID \
+      -e AWS_SECRET_ACCESS_KEY \
+      -e AWS_SESSION_TOKEN \
+      node:$node \
+      bash -c 'cd /app && npm ci && JOBS=max npx --no-install node-pre-gyp build package unpublish publish'
+  done
 done
